d15e76205c404a9790173aa36de0f2efc4d9f337
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-04-28 20:52:46 +0500

    add pg-connect

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,12 +1,21 @@
+const parse = require('pg-connection-string').parse
+
+const { host, port, database, user, password } = parse(process.env.DATABASE_URL)
+
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', ''),
-      password: env('DATABASE_PASSWORD', ''),
+      host,
+      port,
+      database,
+      user,
+      password,
+      //   host: env('DATABASE_HOST', '127.0.0.1'),
+      //   port: env.int('DATABASE_PORT', 5432),
+      //   database: env('DATABASE_NAME', 'strapi'),
+      //   user: env('DATABASE_USERNAME', ''),
+      //   password: env('DATABASE_PASSWORD', ''),
       ssl: {
         ca: env('DATABASE_CA'),
         // rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
